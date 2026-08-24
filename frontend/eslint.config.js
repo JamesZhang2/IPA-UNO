de0cff90c56c767908e391bd4cf2a0846385fe89
JamesZhang2 <jameszhang2019@outlook.com>
2026-08-23 19:11:29 -0700

    Set up ESlint and prettier

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,9 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -19,4 +20,6 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+  // Must stay last so it can turn off the rules that fight with Prettier.
+  eslintConfigPrettier,
+]);
